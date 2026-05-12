7255b2dd24e6349d12259ab6e513784db53ae59c
Malik Halabi <malik.halabi@students.mq.edu.au>
2026-05-12 15:01:33 +1000

    Refactor LiveMatchWidget: Update styles, integrate API key, and enhance match data handling

--- src/components/live-match-widget.js
+++ src/components/live-match-widget.js
@@ -1,32 +1,42 @@
 import { LitElement, html, css } from 'https://cdn.jsdelivr.net/gh/lit/dist@2/core/lit-core.min.js';
-
-class LiveMatchWidget extends LitElement{
+import { API_KEY } from '../config.js';
+class LiveMatchWidget extends LitElement {
     static properties = {
-        header: {type: String},
-        matches: {type: Array},
-        testing: {type: Boolean},
-        loading: {type: Boolean},
-        error: {type:String}
+        header: { type: String },
+        matches: { type: Array },
+        testing: { type: Boolean },
+        loading: { type: Boolean },
+        error: { type: String }
     }
 
 
     static styles = css`
     :host {
         display: block;
-        width: 900px;
+        width: 45vw;
         height: 250px;
+        margin: 1vw 2.5vw 1vw .5vw;
         background-color: azure;
         border-style: solid;
         border-color: rgb(223, 238, 240);
         border-radius: 0 0 5px 5px;
-        margin: 0px 10px 0px 10px;
 
     }
     h4{
         margin-top: 0px;
         padding: 10px;
         border-width: 1px;
+        margin-bottom: 0px;
         
+    }
+        
+    ul{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding-left: 0;
+    flex-direction: column;
+    margin-top: 10px;
     }
     .live-badge{
         display: inline-flex;
@@ -40,14 +50,49 @@ class LiveMatchWidget extends LitElement{
         font-size: small;
         line-height: 1;
         margin-left: x;
+        margin-bottom: 0px;
     }
-
+    li{
+    list-style-type: none;
+    }
     .live-dot{
         width: 8px;
         height: 8px;
         border-radius: 50%;
         background-color: rgb(197, 71, 42);
-    }    
+    }
+
+    .card{
+    background-color: rgb(228, 243, 252);
+    margin: 0px 0px 10px 0px;
+    display: flex;
+    justify-content: center;
+    height: 50px;
+    font-weight: bold;
+    width: 90%;
+    border-radius: 8px;
+    }
+
+    .minute{
+        margin-left: auto;
+    font-size: small;
+    color: rgb(74, 74, 74);
+    font-style: bold;
+    padding-right: 10px;
+    }
+
+    .score {
+    color: rgb(46, 68, 178);
+    font-weight: 700;
+    margin: 15px;
+    font-size: larger;
+    }
+        .card li{
+        display: flex;
+        align-items: center;
+        width: 100%;
+        margin: 0px 0px 0px 15px;
+        }
   `;
 
     constructor() {
@@ -57,39 +102,39 @@ class LiveMatchWidget extends LitElement{
         this.matches = [];
     }
 
-    async getMatches(){
-        try{
-            const response = await fetch(
-                "https://api.football-data.org/v4/matches?status=LIVE",
-                {
-                    method: 'GET'
-                }
-            );
+    async getMatches() {
+        try {
+            const PROXY = 'https://corsproxy.io/?url=';
+            const BASE_URL = 'https://api.football-data.org/v4';
 
-            if (!response.ok){
+            const response = await fetch(`${PROXY}${BASE_URL}/competitions/PL/matches?status=LIVE`, {
+            headers: { 'X-Auth-Token': API_KEY }
+            });
+
+            if (!response.ok) {
                 throw new Error(`HTTP ${response.status}`)
             }
 
-            const data = await(response.json());
+            const data = await (response.json());
             this.matches = data.matches;
         }
-        catch (error){
+        catch (error) {
             console.log("Error", error);
         }
         // If testing mode is enabled, skip the network call and use placeholder data
         if (this.testing) {
             this.matches = [
-                { homeTeam: { shortName: 'ALP' }, awayTeam: { shortName: 'BET' }, minute: '12' },
-                { homeTeam: { shortName: 'GMA' }, awayTeam: { shortName: 'DEL' }, minute: '78' }
+                {score:{fullTime:{home: '3', away: '2'}}, homeTeam: { shortName: 'ALP' }, awayTeam: { shortName: 'BET' }, minute: '12' },
+                { score:{fullTime:{home: '3', away: '2'}},homeTeam: { shortName: 'GMA' }, awayTeam: { shortName: 'DEL' }, minute: '78' }
             ];
         }
-        }
-        connectedCallback(){
-            super.connectedCallback();
-            this.getMatches();
-        }
-        render(){
-            if (this.matches.length >0){
+    }
+    connectedCallback() {
+        super.connectedCallback();
+        this.getMatches();
+    }
+    render() {
+        if (this.matches.length > 0) {
             return html`
             <div>
             <h4>
@@ -98,26 +143,35 @@ class LiveMatchWidget extends LitElement{
              </h4>
                 <ul>
                     ${this.matches.map((match) => html`
+                        <div class = "card">
                         <li>
                             ${match.homeTeam?.shortName ?? 'Home'}
-                            vs
+                            <span class = "score">
+                            ${match.score?.fullTime?.home ?? 0}
+                            -
+                            ${match.score.fullTime.away ?? 0}
+                            </span>
                             ${match.awayTeam?.shortName ?? 'Away'}
-                            (${match.minute ?? '-'}')
+                            <span class="minute">
+                            ${match.minute ?? '-'}'
+                            </span>
                             </li>
+                            </div>
                             `)}
                             </ul>
                             </div>
                             `;
-                    }
+        }
 
-                else{
-                    return html`
+        else {
+            return html`
                     <div>
                         <h3>
                         No Live Matches!</h3>
                         </div>`
-                }}
-        
-
+        }
     }
-    customElements.define('live-match-widget', LiveMatchWidget);
+
+
+}
+customElements.define('live-match-widget', LiveMatchWidget);
